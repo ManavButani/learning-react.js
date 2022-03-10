9c942325e65b72ee89682df4de30e5a33ffa7672
ManavButani <manavkrenil@gmail.com>
2022-03-10 21:16:03 +0530

    Error with a importing liberaries is solved

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,5 +1,5 @@
-import react from 'react'
-import { ReactDOM } from 'react'
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 function Greeting(){// this is react component becasue Greeting G is capital letter
     return <h2>Jai Swaminarayan </h2> // this is not html but it is JSX
