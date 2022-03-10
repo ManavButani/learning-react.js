c59c84bb5c3d0fb3cc61d4193225eb8a93f01cb1
19IT016/19IT016 <19it016@charusat.edu.in>
2022-03-10 21:06:59 +0530

    1first commit

---
+++ tutorial/src/index.js
@@ -0,0 +1,8 @@
+import react from 'react'
+import { ReactDOM } from 'react'
+
+function Greeting(){// this is react component becasue Greeting G is capital letter
+    return <h2>Jai Swaminarayan </h2> // this is not html but it is JSX
+}
+
+ReactDOM.render(<Greeting/>,document.getElementById("root"))
